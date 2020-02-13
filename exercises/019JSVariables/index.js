fd743e547a37418140a1d8bad710b1c87d1c2465
niksavellano <avellanonikko@gmail.com>
2020-02-12 18:02:11 -0700

    added 3 exercises

---
+++ exercises/019JSVariables/index.js
@@ -0,0 +1,17 @@
+const birthmonth = "November";
+const fname = "Nikko";
+const lname = "Avellano";
+
+let city;
+let age;
+let nextcity;
+
+age = 22;
+city = "calgary";
+nextcity = city;
+
+console.log("Month of Birth : ", birthmonth);
+console.log("Name : ", fname + lname);
+console.log("City : ", city);
+console.log("Age : ", age);
+console.log("Next City : ", nextcity);
---
+++ exercises/020JavaScriptConversion/index.js
@@ -0,0 +1,13 @@
+let age = "22";
+let fname = "Nikko";
+let bool = false;
+let finalscore = null;
+let winningscore = null;
+
+console.log(typeof age);
+let num = Number(age);
+console.log(typeof num);
+console.log(typeof fname);
+console.log(typeof bool);
+console.log(typeof finalscore);
+console.log(typeof winningscore);
